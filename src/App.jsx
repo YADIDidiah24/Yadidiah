cde365dd70bb05cf5aa5380284c3729c9d02f481
Yadidiah <yadidiah.wrk@gmail.com>
2025-08-18 15:35:47 +0400

    Add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-
+import { Analytics } from "@vercel/analytics/react"
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
@@ -20,6 +20,7 @@ const App = () => {
           <StarsCanvas />
         </div>
       </div>
+    <Analytics />
     </BrowserRouter>
   );
 }
